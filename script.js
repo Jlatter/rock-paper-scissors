461d540e7d205c457aef87a6dac3d9e635aebf14
Jordan Latter <Jlatter1111@gmail.com>
2022-08-22 23:46:07 +0100

    Make pseudocode for rock paper scissors game

---
+++ script.js
@@ -0,0 +1,10 @@
+//Make a function for computer random rock paper scissor
+function getComputerChoice{ 
+}
+//store computer choice in a variable
+
+//Make a prompt for the player for rock paper scissors and store it in a variable, make it case insensitive
+
+//Make a function to play a round i.e. compare the result of computer's choice and players, and output win lose or tie
+
+//Make a function to keep track of the total number of wins for computer and player, end at 5
