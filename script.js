455a9bd10922bfda35312a00eaa3e11e7ce2058c
Jordan Latter <Jlatter1111@gmail.com>
2022-08-26 18:33:16 +0100

    Create function to randomly select a number between 1 and 3 then assign each number to rock, paper or scissors, create a second function which takes a user input for "rock paper or scissors" and compare it with the result of the first function which then returns a win, loss or tie create a 3rd function to count the number of wins and losses and end at either 5 computer or player wins and display a win message for either

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
-//Make a function for computer random rock paper scissor
-function getComputerChoice{ 
+//Make a function for computer to choose random number out of 1 2 and 3 then assign the number to rock paper, scissors
+function getComputerChoice(){ 
+    let x = Math.floor((Math.random() * 3) + 1);
+    if (x === 1){
+        var computer_choice = "rock";
+    }else if (x === 2){
+         computer_choice = "paper";
+    }else {
+         computer_choice = "scissors";
+    }
+    return computer_choice;
 }
 //store computer choice in a variable
 
+
 //Make a prompt for the player for rock paper scissors and store it in a variable, make it case insensitive
 
+
 //Make a function to play a round i.e. compare the result of computer's choice and players, and output win lose or tie
+function playround(player_lower, comp_choice_variable){
+    comp_choice_variable = getComputerChoice();
+    var player_choice_case = prompt("rock paper or scissors?");
+var player_lower = player_choice_case.toLowerCase();
+    if(player_lower === comp_choice_variable){
+        return("Its a tie!");
+    }else if(player_lower === "rock" && comp_choice_variable === "paper" || player_lower === "paper" && comp_choice_variable === "scissors" || player_lower === "scissors" && comp_choice_variable === "rock"){
+        return("You lose! computer chose " + comp_choice_variable);
+    }else if(player_lower === "rock" && comp_choice_variable === "scissors" || player_lower === "paper" && comp_choice_variable === "rock" || player_lower === "scissors" && comp_choice_variable === "paper"){
+        return("You win! computer chose " + comp_choice_variable);
+    }
+   
+}
 
 //Make a function to keep track of the total number of wins for computer and player, end at 5
+function roundCount(){
+   let keepgoing = true
+   let i = 0;
+   let c = 0;
+while(keepgoing){
+ let rond = playround();
+if (rond.includes("lose")){c++};
+if (rond.includes("win")){i++};
+if(i === 5 || c === 5 ){keepgoing = false};
+console.log( rond + " Player score " + i + " Computer score " + c)
+if(i === 5){console.log("Player Wins")}
+else if(c=== 5){console.log("Computer Wins")}
+}
+
+}
+console.log(roundCount())
