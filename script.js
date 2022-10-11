9b2d60a2a3c3a67aa76b6456d270978747a70082
Jordan Latter <Jlatter1111@gmail.com>
2022-10-11 22:19:25 +0100

    Replace console.log lines with divs that display on screen the results of each round, add buttons to the page and rework the play round function to work with the new divs, add event listeners to each of the buttons to call the playround function.

--- script.js
+++ script.js
@@ -14,37 +14,36 @@ function getComputerChoice(){
 
 
 //Make a prompt for the player for rock paper scissors and store it in a variable, make it case insensitive
-
-
-//Make a function to play a round i.e. compare the result of computer's choice and players, and output win lose or tie
+var playerWins = 0;
+var computerWins = 0;
+var element = document.getElementById("rock");
+var element2 = document.getElementById("paper");
+var element3 = document.getElementById("scissors");
 function playround(player_lower, comp_choice_variable){
     comp_choice_variable = getComputerChoice();
-    var player_choice_case = prompt("rock paper or scissors?");
-var player_lower = player_choice_case.toLowerCase();
     if(player_lower === comp_choice_variable){
-        return("Its a tie!");
-    }else if(player_lower === "rock" && comp_choice_variable === "paper" || player_lower === "paper" && comp_choice_variable === "scissors" || player_lower === "scissors" && comp_choice_variable === "rock"){
-        return("You lose! computer chose " + comp_choice_variable);
-    }else if(player_lower === "rock" && comp_choice_variable === "scissors" || player_lower === "paper" && comp_choice_variable === "rock" || player_lower === "scissors" && comp_choice_variable === "paper"){
-        return("You win! computer chose " + comp_choice_variable);
+        document.getElementById("player").innerHTML = "player wins " + playerWins;
+        document.getElementById("computer").innerHTML = "computer wins " + computerWins;
+        return document.getElementById("demo").innerHTML =("Its a tie!");      
+    }else if(player_lower === "rock" && comp_choice_variable === "paper" || player_lower === "paper" && comp_choice_variable === "scissors"
+     || player_lower === "scissors" && comp_choice_variable === "rock"){
+       computerWins++;
+       if(computerWins === 5){playerWins=0; computerWins = 0; document.getElementById("victory").innerHTML="Sorry you lose"}
+       else{document.getElementById("victory").innerHTML=""};
+       document.getElementById("computer").innerHTML = "computer wins " + computerWins;
+       document.getElementById("player").innerHTML = "player wins " + playerWins;
+        return document.getElementById("demo").innerHTML =("You lose! computer chose " + comp_choice_variable);        
+    }else if(player_lower === "rock" && comp_choice_variable === "scissors" || player_lower === "paper" && comp_choice_variable === "rock" 
+    || player_lower === "scissors" && comp_choice_variable === "paper"){
+        playerWins++;
+        if(playerWins === 5){playerWins=0; computerWins = 0; document.getElementById("victory").innerHTML="Congratulations you win!"}
+        else{document.getElementById("victory").innerHTML=""};
+        document.getElementById("player").innerHTML = "player wins " + playerWins;
+        document.getElementById("computer").innerHTML = "computer wins " + computerWins;
+        return document.getElementById("demo").innerHTML =("You win! computer chose " + comp_choice_variable);
     }
-   
-}
+};  
 
-//Make a function to keep track of the total number of wins for computer and player, end at 5
-function roundCount(){
-   let keepgoing = true
-   let i = 0;
-   let c = 0;
-while(keepgoing){
- let rond = playround();
-if (rond.includes("lose")){c++};
-if (rond.includes("win")){i++};
-if(i === 5 || c === 5 ){keepgoing = false};
-console.log( rond + " Player score " + i + " Computer score " + c)
-if(i === 5){console.log("Player Wins")}
-else if(c=== 5){console.log("Computer Wins")}
-}
-
-}
-console.log(roundCount())
+element.addEventListener("click", event => playround("rock"));
+element2.addEventListener("click", event => playround("paper"));
+element3.addEventListener("click", event => playround("scissors"));
